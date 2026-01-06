8cde7f3734319f0139909cd7ec1518f9944a7ec3
Darshan Wadile <darshanwadile10@gmail.com>
2026-01-06 10:28:25 +0530

    Final Commit

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { Navigation } from './Navigation';
-export { Hero } from './Hero';
-export { About } from './About';
-export { Portfolio } from './Portfolio';
-export { Contact } from './Contact';
-export { Footer } from './Footer';
-export { LoadingScreen } from './LoadingScreen';
-
